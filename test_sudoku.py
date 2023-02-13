07d0fca5f2f003fa7a9bc717bb72449ad167ff79
j0zeph <notaj@tutanota.de>
2023-02-13 15:10:02 +0300

    Add tests for sudoku utils

--- test_sudoku.py
+++ test_sudoku.py
@@ -1,20 +1,62 @@
 import pytest
 from sudoku import num_has_row_copy
+import sudoku_utils
+
 
 def test_number_with_a_row_copy_is_caught():
     faulty_grid = [
-    '.6.8..5.6',
-    '..5...367',
-    '37..658.9',
-    '6.9..21..',
-    '..14892.4',
-    '...3.69..',
-    '.5....4..',
-    '.1.547..3',
-    '.96.38..1',
-]
+        '.6.8..5.6',
+        '..5...367',
+        '37..658.9',
+        '6.9..21..',
+        '..14892.4',
+        '...3.69..',
+        '.5....4..',
+        '.1.547..3',
+        '.96.38..1',
+    ]
 
-    assert num_has_row_copy((0,1), faulty_grid)
-    assert num_has_row_copy((0,8), faulty_grid)
-    assert num_has_row_copy((4,3), faulty_grid)
-    assert num_has_row_copy((4,8), faulty_grid)
+    assert num_has_row_copy((0, 1), faulty_grid)
+    assert num_has_row_copy((0, 8), faulty_grid)
+    assert num_has_row_copy((4, 3), faulty_grid)
+    assert num_has_row_copy((4, 8), faulty_grid)
+
+
+@pytest.mark.parametrize(
+    'coordinate_moves, actual_moves',
+    [
+        ('2A1', ((0, 0), 2)),
+        ('21a', ((0, 0), 2)),
+        ('7i3', ((8, 2), 7)),
+        ('6E8', ((4, 7), 6)),
+        ('68e', ((4, 7), 6)),
+    ],
+)
+def test_coordinates_are_translated_into_moves(coordinate_moves, actual_moves):
+    assert actual_moves == sudoku_utils.translate_move(coordinate_moves)
+
+
+def test_coordinates_that_are_longer_or_shorter_than_expected_are_caught():
+    expected_message = 'Coordinate is invalid.\nThe legal format is <number><row number><column letter> or <number><column letter><row number>'
+    with pytest.raises(sudoku_utils.SudokuError, match=expected_message):
+        sudoku_utils.translate_move('1I89')
+        sudoku_utils.translate_move('21a4')
+        sudoku_utils.translate_move('1a4')
+
+
+def test_invalid_sudoku_number_in_coordinate_is_caught():
+    with pytest.raises(sudoku_utils.SudokuError, match='Only numbers from 1 to 8 can be placed into the grid.'):
+        sudoku_utils.translate_move('0A1')
+        sudoku_utils.translate_move('#E8')
+
+
+def test_invalid_row_in_coordinate_is_caught():
+    with pytest.raises(sudoku_utils.SudokuError, match='Only row letters from A to I are allowed.'):
+        sudoku_utils.translate_move('2J3')
+        sudoku_utils.translate_move('89R')
+
+
+def test_invalid_column_in_coordinate_is_caught():
+    with pytest.raises(sudoku_utils.SudokuError, match='Only column numbers from 1 to 9 are allowed.'):
+        sudoku_utils.translate_move('3A0')
+        sudoku_utils.translate_move('80I')
