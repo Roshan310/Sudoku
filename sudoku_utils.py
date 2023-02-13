9b9931bd9dfbf8a642c244adc6b765d27bfedf2c
j0zeph <notaj@tutanota.de>
2023-02-13 15:07:49 +0300

    Add file with general sudoku utility functions

---
+++ sudoku_utils.py
@@ -0,0 +1,63 @@
+"""General utility functions for the Sudoku grid"""
+
+from typing import Tuple
+
+LEGAL_COORDINATE_LENGTH = 3
+VALID_COLS = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+VALID_ROWS = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'}
+
+class SudokuError(Exception):
+    """General exception class for the Sudoku game"""
+
+    def __init__(self, message='Something went wrong. Please try again') -> None:
+        self.error_message = message
+        super().__init__(self.error_message)
+
+
+def translate_move(coordinate_move: str) -> Tuple[Tuple[int, int], int]:
+    """Returns a tuple that is the result of translating Sudoku coordinates into moves.
+
+    The coordinate_move `9a8` or `9A8` or `98a` or `98A` asks to place a `9` in the intersection between 
+    row `A` and column `8`.
+    
+    The tuple ((0, 7), 9) will be returned, where the first tuple (0, 7) denotes row `0`, column `7`, and the last number
+    denotes the `9` that will be placed in that location.
+    """
+    # rows that each letter corresponds to
+    letter_rows = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8}
+
+    if not len(coordinate_move) == LEGAL_COORDINATE_LENGTH:
+        raise SudokuError(
+            'Coordinate is invalid.\nThe legal format is <number><row number><column letter> or <number><column letter><row number>'
+        )
+
+    if not (coordinate_move[0].isdecimal() and 1 <= int(coordinate_move[0]) <= 8):
+        raise SudokuError('Only numbers from 1 to 8 can be placed into the grid.')
+    
+    number = int(coordinate_move[0])
+
+    if (coordinate_move[1].isdecimal()): # the numeric column was provided first
+        
+        column = int(coordinate_move[1])
+    
+        if column not in VALID_COLS:
+            raise SudokuError('Only column numbers from 1 to 9 are allowed.')
+
+        row = str(coordinate_move[2]).upper()
+
+        if not (row.isalpha() and row in VALID_ROWS):
+            raise SudokuError('Only row letters from A to I are allowed.')
+
+        return ((letter_rows[row], column - 1), number)
+
+    else: # the alphabetical row was provided first
+        _row = coordinate_move[1].upper()
+
+        if not _row in VALID_ROWS:
+            raise SudokuError('Only row letters from A to I are allowed.')
+        else:
+            _column = coordinate_move[2]
+            if not (_column.isdigit() and int(_column) in VALID_COLS):
+                raise SudokuError('Only column numbers from 1 to 9 are allowed.')
+            
+            return ((letter_rows[_row], int(_column) - 1), number)
